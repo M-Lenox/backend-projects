a6a9ab3d6a41e008ef15380ea6e586e840fc9e65
Lenox <lenoxmangi979@gmail.com>
2025-04-22 11:40:40 +0300

    add a fuctionaluty - list all tasks

--- task-tracker-cli/taskManager.js
+++ task-tracker-cli/taskManager.js
@@ -6,17 +6,6 @@ const filePath = path.join(__dirname + "/tasks.json");
 const date = new Date();
 const time = `${date.getHours()}hrs and ${date.getMinutes()} minutes past midnight.`;
 
-//List all tasks.
-function list() {
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.log("No file to list your tasks\n", err);
-    }
-
-    console.log(data);
-  });
-}
-
 //Add a new task
 function add(taskDescription) {
   fs.readFile(filePath, "utf8", (err, data) => {
@@ -51,6 +40,25 @@ function add(taskDescription) {
         if (err) throw err;
         console.log("Task successfully added.");
       });
+    }
+  });
+}
+
+//List all tasks.
+function list() {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.log("No file to list your tasks\n", err);
+    } else {
+      console.log("id    Task");
+
+      let allData = JSON.parse(data);
+
+      let i = 0;
+      while (i < allData.length) {
+        console.log(`${allData[i].id}    ${allData[i].description}`);
+        i += 1;
+      }
     }
   });
 }
