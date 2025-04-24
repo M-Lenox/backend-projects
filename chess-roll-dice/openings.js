759127904a22ccf8139599cd8ec9f9c24b34d42a
Lenox <lenoxmangi979@gmail.com>
2025-04-24 13:36:02 +0300

    ask the user the color they are playing and randomly choose a chess opening based on the color

--- chess-roll-dice/openings.js
+++ chess-roll-dice/openings.js
@@ -1,4 +1,6 @@
-const white = {
+//Openings for white
+const whiteOpenings = {
+  0: "Play the Bird opening",
   1: "Play a gambit line",
   2: "Play a dubious opening and survive the trouble",
   3: "Play the closed sicilian",
@@ -16,8 +18,10 @@ const white = {
   15: "Play the Ruy Lopez against 1...e5",
 };
 
-const black = {
+//Openings for black
+const blackOpenings = {
   e4: {
+    0: "Play the caro-kann opening",
     1: "Play the French defence.",
     2: "Play the Petroff defence",
     3: "Play the Najdorf Variation of the Sicilian defence",
@@ -26,41 +30,94 @@ const black = {
     6: "Play the Pirc or Modern defence",
   },
   d4: {
+    0: "Play 1...b6",
     1: "Play the Lasker variation of the Queen's gambit declined.",
     2: "Accept the gambit ie Queen's gambit",
     3: "Play the King's Indian Defence",
     4: "Play the Grunfeld defence",
   },
   c4: {
+    0: "Play 1...c5",
     1: "Play 1...e5",
     2: "Play 1...e6",
-    3: "Play 1...Nf6 KID",
+    3: "Play 1...Nf6, the King's Indian Defence",
     4: "Play a dubious 1st move",
   },
-  others: "Have fun, it is never that serious.",
+  others: {
+    0: "Play 1.a3/a6",
+    1: "Play 1.a4/a5",
+    2: "Play 1.b3/b6",
+    3: "Play 1.b4/b5",
+    4: "Play 1.c3/c6",
+    5: "Play 1.c4/c5",
+    6: "Play 1.d3/d6",
+    7: "Play 1.d4/d5",
+    8: "Play 1.e3/e6",
+    9: "Play 1.e4/e5",
+    10: "Play 1.f3/f6",
+    11: "Play 1.f4/f5",
+    12: "Play 1.g3/g6",
+    13: "Play 1.g4/g5",
+    14: "Play 1.h3/h6",
+    15: "Play 1.h4/h5",
+    16: "Play 1.Na3/Na6",
+    17: "Play 1.Nc3/Nc6",
+    18: "Play 1.Nf3/Nf6",
+    19: "Play 1.Nh3/Nh6",
+  },
 };
 
-const fun = {
-  1: "a3/a6",
-  2: "a4/a5",
-  3: "b3/b6",
-  4: "b4/b5",
-  5: "c3/c6",
-  6: "c4/c5",
-  7: "d3/d6",
-  8: "d4/d5",
-  9: "e3/e6",
-  10: "e4/e5",
-  11: "f3/f6",
-  12: "f4/f5",
-  13: "g3/g6",
-  14: "g4/g5",
-  15: "h3/h6",
-  16: "h4/h5",
-  17: "Na3/Na6",
-  18: "Nc3/Nc6",
-  19: "Nf3/Nf6",
-  20: "Nh3/Nh6",
+//Random first legal moves
+const randomFirstMoves = {
+  1: "Play 1.a3/a6",
+  2: "Play 1.a4/a5",
+  3: "Play 1.b3/b6",
+  4: "Play 1.b4/b5",
+  5: "Play 1.c3/c6",
+  6: "Play 1.c4/c5",
+  7: "Play 1.d3/d6",
+  8: "Play 1.d4/d5",
+  9: "Play 1.e3/e6",
+  10: "Play 1.e4/e5",
+  11: "Play 1.f3/f6",
+  12: "Play 1.f4/f5",
+  13: "Play 1.g3/g6",
+  14: "Play 1.g4/g5",
+  15: "Play 1.h3/h6",
+  16: "Play 1.h4/h5",
+  17: "Play 1.Na3/Na6",
+  18: "Play 1.Nc3/Nc6",
+  19: "Play 1.Nf3/Nf6",
+  20: "Play 1.Nh3/Nh6",
 };
 
-module.exports = { white, black, fun };
+//pick a random white opening
+function white() {
+  const randomWhiteOpening = function () {
+    const keys = Object.keys(whiteOpenings);
+    return whiteOpenings[keys[(keys.length * Math.random()) << 0]];
+  };
+  console.log(">> ", randomWhiteOpening());
+}
+
+//pick a random black opening
+function black(firstMove) {
+  const randomBlackOpening = function () {
+    const whiteMove = firstMove;
+    const keys = Object.keys(blackOpenings[whiteMove]);
+
+    const blackMove = keys[(keys.length * Math.random()) << 0];
+    return blackOpenings[whiteMove][blackMove];
+  };
+  console.log(">> ", randomBlackOpening());
+}
+
+//pick a random first move
+function randomFirstMove() {
+  const randomOpening = function () {
+    const keys = Object.keys(randomFirstMoves);
+    return randomFirstMoves[keys[(keys.length * Math.random()) << 0]];
+  };
+  console.log(">> ", randomOpening());
+}
+module.exports = { white, black, randomFirstMove };
---
+++ chess-roll-dice/randomizer.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+const { intro, select, isCancel, cancel } = require("@clack/prompts");
+const { white, black, randomFirstMove } = require("./openings");
+const pc = require("picocolors");
+
+async function randomizer() {
+  intro(pc.inverse("\n roll-the-chess-opening-dice "));
+
+  const colorPlay = await select({
+    message: "What color are you playing?",
+    options: [
+      { value: "white", label: "White." },
+      { value: "black", label: "Black." },
+      {
+        value: "randomFirstMove",
+        label: "Have fun! with a random first move.",
+      },
+    ],
+  });
+
+  if (isCancel(colorPlay)) {
+    cancel("Operation cancelled.");
+    return process.exit(0);
+  }
+
+  switch (colorPlay) {
+    case "white":
+      white();
+      break;
+
+    case "black":
+      const firstMove = await select({
+        message: "What did white play?",
+        options: [
+          { value: "e4", label: "1. e4" },
+          { value: "d4", label: "1. d4" },
+          { value: "c4", label: "1. c4" },
+          { value: "others", label: "others" },
+        ],
+      });
+
+      black(firstMove);
+
+      if (isCancel(firstMove)) {
+        cancel("Operation cancelled.");
+        return process.exit(0);
+      }
+      break;
+
+    case "randomFirstMove":
+      randomFirstMove();
+      break;
+
+    default:
+      console.log("There was an error with the app.");
+  }
+  setTimeout(randomizer, 3000);
+}
+randomizer();
